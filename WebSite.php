58ab5edc27287c7c35400882277514e898005807
Gonzvp <43019932+Gonzvp@users.noreply.github.com>
2019-02-12 10:06:28 -0600

    test

---
+++ WebSite.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head itemscope itemtype="http://schema.org/WebSite">
+	
+	<meta name="description" content="">
+	<title></title>
+
+	<meta property="og:title" content="">
+	<meta property="og:url" content="">
+	<meta property="og:description" content="">
+	<meta property="og:image" content="">
+	<meta property="og:type" content="">
+
+	<meta property="fb:app_id" content="">
+
+	<meta name="twitter:domain" content="">
+	<meta name="twitter:card" content="summary">
+	<meta name="twitter:title" content="">
+	<meta name="twitter:url" content="">
+	<meta name="twitter:description" content="">
+	<meta name="twitter:image" content="">
+
+	<script type="application/ld+json">
+		{
+			"@context":"http://schema.org",
+      		"@type":"WebSite",
+      		"name":"",
+      		"alternateName":"",
+      		"url":"",
+      		"image":"",
+      		"description":"",
+      		"keywords":"",
+      		"creator":""
+		}
+	</script>
+
+	<script type="application/ld+json">
+		{
+  			"@context": "http://schema.org",
+  			"@graph":
+  			[	
+  				{
+       				"@type": "WebSite",
+       				"name": "SELPI",
+       				"url":"https://www.ipservices.com.mx/",
+       				"image":"https://www.ipservices.com.mx/images/optimized/selpi3_p.webp",
+       				"description":"Somos una firma de abogados especializada en derecho intelectual, rama del derecho internacional ocupada de la protección legal de marcas, avisos comerciales y diseños, obtencionde patentes, diseños industriales, modelos de utilidad, derechos de autor, secretos industriales, nombres de dominio, contratos de licencia y de franquicia.",
+       				"keywords":"registro, marca",
+       				"creator":"Grupo Sincro"
+
+    			},
+    			{
+       				"@type": "LegalService",
+       				"legalName":"Asesoria Legal en Propiedad Intelectual",
+      				"address":
+      					{
+        					"@type":"PostalAddress",
+            				"streetAddress":"calle",
+            				"addressLocality":"municipio",
+            				"addressRegion":"estado",
+            				"postalCode":"55010",
+            				"addressCountry":"MX"
+          				},
+      				"isAccessibleForFree": false,
+      				"currenciesAccepted": "MXN, USD",
+      				"paymentAccepted":"Efectivo, Tarjeta de Credito o Debito",
+    			}
+  			]
+		}
+	</script>
+
+</head>
+<body>
+
+</body>
+</html>
